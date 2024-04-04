84f58973c438dbd7a667a83acee0285d024887d6
zinckle <mzinck9@gmail.com>
2024-04-04 13:47:15 -0400

    moved some stuff around

--- TrainingPrograms/TrainLetterDetect.py
+++ TrainingPrograms/TrainLetterDetect.py
@@ -9,30 +9,7 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 
 #test_data = "../Dataset/letter_mnist_test/emnist-balanced-test.csv"
-train_data = "../Dataset/letter_mnist_train/emnist-letters-train.csv"
 
-readDF = pd.read_csv(train_data)
-#test_df = pd.read_csv(test_data)
-train_df, test_df = train_test_split(readDF, test_size=0.2)
-
-y_train = train_df['label']
-y_test = test_df['label']
-del train_df['label']
-del test_df['label']
-
-from sklearn.preprocessing import LabelBinarizer
-label_binarizer = LabelBinarizer()
-y_train = label_binarizer.fit_transform(y_train)
-y_test = label_binarizer.fit_transform(y_test)
-
-x_train = train_df.values
-x_test = test_df.values
-
-x_train = x_train / 255
-x_test = x_test / 255
-
-x_train = x_train.reshape(-1,28,28,1)
-x_test = x_test.reshape(-1,28,28,1)
 
 datagen = ImageDataGenerator(
         featurewise_center=False,
